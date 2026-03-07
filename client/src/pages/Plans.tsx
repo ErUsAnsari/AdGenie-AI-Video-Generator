598a89f8329e6e3f145283ee49cdcdedaf0abfc2
Usman Ansari <usmanansari2688@gmail.com>
2026-03-07 16:41:16 +0530

    06 Plans and Loading Page

--- client/src/pages/Plans.tsx
+++ client/src/pages/Plans.tsx
@@ -1,9 +1,19 @@
-
+import Pricing from "../components/Pricing";
 
 const Plans = () => {
   return (
-    <div>Plans</div>
-  )
-}
+    <div className="max-sm:py-10 sm:pt-20">
+      <Pricing />
 
-export default Plans
+      <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">
+        Create stunning images for just{" "}
+        <span className="font-medium text-indigo-400">5 credits</span> per image
+        and generate immersive videos for{" "}
+        <span className="font-medium text-indigo-400">10 credits</span> per
+        video.
+      </p>
+    </div>
+  );
+};
+
+export default Plans;
